d48632c5630fd851633ad01e48befd005a0ca3c0
felipeuba <165933197+felipeuba@users.noreply.github.com>
2024-05-27 13:58:40 -0300

    Update simulacro.hs

--- simulacro.hs
+++ simulacro.hs
@@ -69,3 +69,69 @@ amigosDe e (x:xs) | e == fst x = [snd x] ++ amigosDe e xs
                   | e == snd x = [fst x] ++ amigosDe e xs
                   | otherwise = amigosDe e xs
 
+
+import random
+
+def poner_pos_pares_en_cero (s:list[int]) -> list[int]:
+    res:list[int] = s.copy()
+    for i in range(0,len(s),1):
+        if i % 2 == 0:
+            res[i] = 0
+    return res
+
+def solicitar_alumnos () -> list[str]:
+    res:list[str] = []
+    siga_ingresando:bool = True
+    while siga_ingresando:
+        nombre = input("Ingrese el nombre del alumno")
+        if nombre != "listo":
+            res.append(nombre)
+        else:
+            siga_ingresando = False
+    return res
+
+
+
+def monedero_electronico () -> list[tuple[str,float]]:
+    res:list [list[str,float]] = []
+    siga_ingresando:bool = True
+    while siga_ingresando:
+        opcion = input("Ingrese C para cargar, D para descontar o X para salir \n")
+        if opcion == "C":
+            res.append(("C",float(input("Cuanto quiere cargar \n"))))
+        elif opcion == "D":
+            res.append(("D",float(input("Cuanto quiere descontar \n"))))
+        elif opcion == "X":
+            siga_ingresando = False
+        else:
+            print("ingresa lo que te digo flaco")
+    return res
+
+#print(monedero_electronico())
+
+cartas:list[float] = [1,2,3,4,5,6,7,10,11,12]
+repartir:float = random.choice(cartas)
+def siete_y_medio_aux (num:int) -> float:
+    if num < 8:
+        return num
+    else:
+        return 0.5
+
+def siete_y_medio () -> list[int]:
+    puntuacion:float = 0
+    seguir:bool = True
+    while seguir:
+        opcion:str = input("quiere seguir jugando si/no \n")
+        if opcion == "si":
+            puntuacion += siete_y_medio_aux(repartir)
+            if puntuacion >= 7.5:
+                seguir = False
+        elif opcion == "no":
+            seguir = False
+    if puntuacion == 7.5:
+        return f"Ganaste: {puntuacion}"
+    else:
+    return f"Perdiste: {puntuacion}"
+
+print(siete_y_medio())
+
